be26a5dfa3dbc42494f6f3805a76fdb4c91ed06d
Santhosh Kumar <sku272@sapient.com>
2017-07-15 21:39:41 +0530

    Webpack Image and style added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
 	var element =document.createElement('div');
 
-	element.innerHTML = _.join(['Hello', 'World'], ' ');
+	element.innerHTML = _.join(['Hello', 'World !!.'], ' ');
+	element.classList.add('hello');
+
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
 
 	return element;
 }
